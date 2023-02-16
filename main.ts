6c40a47e9e5c3980940501685e277e41b8b8debd
neusfer <125355473+neusfer@users.noreply.github.com>
2023-02-16 09:32:13 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+let dado = 0
+input.onGesture(Gesture.Shake, function () {
+    dado = randint(0, 10)
+    if (6 == dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    } else if (5 == dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (4 == dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (3 == dado) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (2 == dado) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (1 == dado) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+    	
+    }
+})
